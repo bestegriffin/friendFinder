250c883b24ce0260782605295ec89f16a061f826
bestegriffin <griffinbeste@gmail.com>
2017-10-20 22:13:50 -0400

    Add files via upload

---
+++ friends.js
@@ -0,0 +1,28 @@
+var friendArray = [
+  {
+  "name":"Walter",
+  "photo":"http://vignette4.wikia.nocookie.net/breakingbad/images/e/e7/BB-S5B-Walt-590.jpg/revision/latest?cb=20130928055404",
+  "scores":[
+     5,
+     1
+   ]
+ },  {
+  "name":"Jesse",
+  "photo":"https://vignette2.wikia.nocookie.net/breakingbad/images/f/f2/Jesse_Pinkman2.jpg/revision/latest?cb=20170418090449",
+  "scores":[
+     1,
+     5
+   ]
+ },  {
+  "name":"Badger",
+  "photo":"https://vignette.wikia.nocookie.net/breakingbad/images/5/5b/Cast_bb_800x600_badger.jpg/revision/latest?cb=20170613184551",
+  "scores":[
+     3,
+     3
+   ]
+ }
+];
+
+
+// export the array. 
+module.exports = friendArray;
